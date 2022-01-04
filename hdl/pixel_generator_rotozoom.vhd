fb1b43008a31ac3ad32a371d2f1dfb19d2c54e83
François Galea <francois.galea@cea.fr>
2022-01-04 20:33:03 +0100

    Bumped copyright years.

--- hdl/pixel_generator_rotozoom.vhd
+++ hdl/pixel_generator_rotozoom.vhd
@@ -1,6 +1,6 @@
 -- pixel_generator_rotozoom.vhd - Rotozoom pixel generator
 --
--- Copyright (c) 2021 Francois Galea <fgalea at free.fr>
+-- Copyright (c) 2021-2022 Francois Galea <fgalea at free.fr>
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
 -- the Free Software Foundation, either version 3 of the License, or
